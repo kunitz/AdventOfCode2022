24951859e2af485562cf665ebd4831524d07f896
Logan Kunitz <logan.kunitz@ni.com>
2022-12-05 12:38:45 -0600

    Solved problem 5.1

--- python/main.py
+++ python/main.py
@@ -52,8 +52,68 @@ def problem42():
             print(f'{row}')
     print(f'{count}')
 
+class P51_data():
+    def __init__(self, data):
+        self.header = data[0]
+        self.moves = data[1]
+        self.parseHeader()
+        self.executeMoves()
+
+    def parseHeader(self):
+        rows = self.header.split('\n')
+        lastRow = rows.pop()
+        boxes = lastRow.split('   ')
+        self.stack = []
+        for b in boxes:
+            b = b.strip()
+            self.stack.append([])
+        numRows = len(rows)
+        i = 0
+        while i < numRows:
+            r = rows.pop()
+            for b in boxes:
+                box = r[((int(b)-1)*4)+1]
+                j = int(b) - 1
+                if box != ' ':
+                    self.stack[int(b)-1].append(box)
+            i+=1
+
+    def moveStack(self, quantity, fromstack, tostack):
+        i = 0
+        fromstack = fromstack - 1
+        tostack = tostack - 1
+        while i < quantity:
+            i+=1
+            box = self.stack[fromstack].pop()
+            self.stack[tostack].append(box)
+
+    def getCrates(self):
+        crates = ''
+        for s in self.stack:
+            if len(s) > 0:
+                crates += s[len(s)-1]
+            else:
+                crates += ' '
+        return crates
+
+    def executeMoves(self):
+        moverows = self.moves.split('\n')
+        for m in moverows:
+            move_commands = m.split(' ')
+            quantity = int(move_commands[1])
+            stackfrom = int(move_commands[3])
+            stackto = int(move_commands[5])
+            self.moveStack(quantity, stackfrom, stackto)
+            #print(f'{self.getCrates()}')
+
+
+
+def problem51():
+    data = P51_data(read_file('5-1.input').split('\n\n'))
+    print(f'{data.getCrates()}')
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    problem42()
+    problem51()
 
 
